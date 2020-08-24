86f307c09e078307d05985b397aa7aa88a39a90b
wakamiku225 <m0118274cd@edu.teu.ac.jp>
2020-08-24 13:49:15 +0900

    kaooo

--- last/home.js
+++ last/home.js
@@ -1,5 +1,5 @@
 
-fetch('https://graph.facebook.com/v7.0/2572249166435402/accounts?access_token=EAAK5fdPk3ckBAA9SWVql7ZAswZBlcEOtaWjB3kAbunYlDR2H3y7Wy3E4FQToGCmY5xxwZAHNSeDfuWJJjqNvc9Tq3H54EpiVDhZBdZBx137mtJIn7TTb8bH0TplaGShQ4n2HFftk3LSzWbwjZBbVlXlhriJTe22JAisSFKZAJ5PIQZDZD')
+fetch('https://graph.facebook.com/v8.0/17841427624640862?fields=name,username,profile_picture_url,media_count,followers_count,follows_count,media.limit(5){caption,like_count,media_url,permalink,timestamp,thumbnail_url}&access_token=EAAK5fdPk3ckBAMZCvZBK73SEZC0pMLFZCZAZAkMwJR8QY9Y2VPZCihZCqDflxtDtw6xP7F7EvvjvvcG1fZCpmWXkyknem5nia40J0JoZA0XxdZAYT6m5CpZA2dJT90QKfyXwPzENlPmXIQqrKsijd0Im7F4XZCxXVK9MTw5y12PyoZCMm1gQZDZD')
       .then(function (res){
         console.log(res);
         return res.json();
